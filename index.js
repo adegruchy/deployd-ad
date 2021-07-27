3d4e63c82487c262c39d4232b18fc85b3cd00096
adegruchy <averydegruchy@gmail.com>
2021-07-27 17:10:10 +0000

    Applied local updates made

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 var activeDirectory = require('activedirectory');
 
-module.exports = function(username, password, domain, activeDirectoryGroup, callback) {
+module.exports = function(username, password, domain, url, activeDirectoryGroup, callback) {
 
-    var domainURL = 'LDAP://' + domain;
+	var baseUsername = username.toString();
+    var domainURL = 'LDAP://' + url;
     if(username.indexOf('@') == -1) username = username.toString() + '@' + domain;
-
+	
     var ad = new activeDirectory({
-        url: 'LDAP://' + domain,
+        url: domainURL,
         baseDN: 'dc=' + domain.split(".")[0] + ',dc=' + domain.split(".")[1],
         username: username,
         password: password
@@ -19,11 +20,23 @@ module.exports = function(username, password, domain, activeDirectoryGroup, call
     }
 
     // Check if the user is part of the active directory group
-    ad.isUserMemberOf(username, activeDirectoryGroup, function(err, isMember) {
+    ad.getUsersForGroup(activeDirectoryGroup, function(err, users) {
         if (err) {
+			console.log("deployd-ad error:", err);
             callback(false);
         } else {
-            callback(typeof isMember !== 'undefined' && isMember === true);
+			var foundUser = false;
+			for(var i = 0; i < users.length; i++) {
+				if(users[i].sAMAccountName && users[i].sAMAccountName.toLowerCase() === baseUsername.toLowerCase()) {
+					foundUser = true;
+					break;
+				}
+				if(users[i].userPrincipalName && users[i].userPrincipalName.toLowerCase().indexOf(baseUsername.toLowerCase()) != -1) {
+					foundUser = true;
+					break;
+				}
+			}
+			callback(foundUser);
         }
     });
 };
